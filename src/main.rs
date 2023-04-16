d9aa8de3441ba908fb28eb81194f4a69772746e8
Elmer Gonzalez <gonzalezelmerf@gmail.com>
2023-04-16 14:15:14 -0400

    init commit

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+use std::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let listener = TcpListener::bind("127.0.0.1:8000")
+        .expect("Failed to bind random port.");
+
+    tag_api::run(listener)?.await
+}
