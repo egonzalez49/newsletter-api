cac6e9a441092a8e958b650875b819cb87aa81a3
Elmer Gonzalez <gonzalezelmerf@gmail.com>
2023-04-17 21:07:14 -0400

    refactor into crates

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use std::net::TcpListener;
+use tag_api::{startup::run, configuration::get_configuration};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let listener = TcpListener::bind("127.0.0.1:8000")
-        .expect("Failed to bind random port.");
+    let configuration = get_configuration().expect("Failed to read configuration.");
 
-    tag_api::run(listener)?.await
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
+
+    run(listener)?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,11 +1,12 @@
 use std::net::TcpListener;
+use tag_api::startup::run;
 
 fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0")
         .expect("Failed to bind random port");
 
     let port = listener.local_addr().unwrap().port();
-    let server = tag_api::run(listener).expect("Failed to bind address");
+    let server = run(listener).expect("Failed to bind address");
     let _ = tokio::spawn(server);
 
     format!("http://127.0.0.1:{}", port)
