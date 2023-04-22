b09e9e29f8a1902d24492194f71b032d61944775
Elmer Gonzalez <gonzalezelmerf@gmail.com>
2023-04-22 13:48:24 -0400

    use connection pool when connecting to database

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
 use std::net::TcpListener;
+use sqlx::PgPool;
 use tag_api::{startup::run, configuration::get_configuration};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection_pool = PgPool::connect(
+            &configuration.database.connection_string()
+        )
+        .await
+        .expect("Failed to connect to database.");
 
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
 
-    run(listener)?.await
+    run(listener, connection_pool)?.await
 }
