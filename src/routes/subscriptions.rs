4cf184e5ee1facee76c42b918f803e185ce7257e
Elmer Gonzalez <gonzalezelmerf@gmail.com>
2023-06-08 20:28:01 -0400

    (¬_¬) fix annoying little bug

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -10,7 +10,7 @@ impl TryFrom<FormData> for NewSubscriber {
 
     fn try_from(value: FormData) -> Result<Self, Self::Error> {
         let name = SubscriberName::parse(value.name)?;
-        let email = SubscriberEmail::parse(form.email)?;
+        let email = SubscriberEmail::parse(value.email)?;
         Ok(NewSubscriber { email, name })
     }
 }
