b43d310489abdc232831bc21e77a4f19657e5234
Elmer Gonzalez <gonzalezelmerf@gmail.com>
2023-07-16 18:12:57 -0400

    implement newsletter endpoint and tests

--- src/startup.rs
+++ src/startup.rs
@@ -1,24 +1,26 @@
-use crate::{
-    configuration::{DatabaseSettings, Settings},
-    email_client::EmailClient,
-    routes::{confirm, health_check, subscribe},
+use actix_web::{
+    dev::Server,
+    web::{self, Data},
+    App, HttpServer,
 };
-use actix_web::{dev::Server, web, App, HttpServer};
 use sqlx::{postgres::PgPoolOptions, PgPool};
 use std::net::TcpListener;
 use tracing_actix_web::TracingLogger;
+
+use crate::{
+    configuration::{DatabaseSettings, Settings},
+    email_client::EmailClient,
+    routes::{confirm, health_check, publish_newsletter, subscribe},
+};
 
 pub struct Application {
     port: u16,
     server: Server,
 }
 
-pub struct ApplicationBaseUrl(pub String);
-
 impl Application {
     pub async fn build(configuration: Settings) -> Result<Self, std::io::Error> {
         let connection_pool = get_connection_pool(&configuration.database);
-
         let sender_email = configuration
             .email_client
             .sender()
@@ -62,27 +64,29 @@ pub fn get_connection_pool(configuration: &DatabaseSettings) -> PgPool {
         .connect_lazy_with(configuration.with_db())
 }
 
-pub fn run(
+pub struct ApplicationBaseUrl(pub String);
+
+fn run(
     listener: TcpListener,
     db_pool: PgPool,
     email_client: EmailClient,
     base_url: String,
 ) -> Result<Server, std::io::Error> {
-    let db_pool = web::Data::new(db_pool);
-    let email_client = web::Data::new(email_client);
-    let base_url = web::Data::new(ApplicationBaseUrl(base_url));
+    let db_pool = Data::new(db_pool);
+    let email_client = Data::new(email_client);
+    let base_url = Data::new(ApplicationBaseUrl(base_url));
     let server = HttpServer::new(move || {
         App::new()
             .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .route("/subscriptions/confirm", web::get().to(confirm))
+            .route("/newsletters", web::post().to(publish_newsletter))
             .app_data(db_pool.clone())
             .app_data(email_client.clone())
             .app_data(base_url.clone())
     })
     .listen(listener)?
     .run();
-
     Ok(server)
 }
