d89531426f02964a284436ca628943966845d183
Elmer Gonzalez <gonzalezelmerf@gmail.com>
2023-08-12 11:44:22 -0400

    implement password change and logout functionality

--- src/routes/admin/dashboard.rs
+++ src/routes/admin/dashboard.rs
@@ -4,14 +4,7 @@ use reqwest::header::LOCATION;
 use sqlx::PgPool;
 use uuid::Uuid;
 
-use crate::session_state::TypedSession;
-
-fn e500<T>(e: T) -> actix_web::Error
-where
-    T: std::fmt::Debug + std::fmt::Display + 'static,
-{
-    actix_web::error::ErrorInternalServerError(e)
-}
+use crate::{session_state::TypedSession, utils::e500};
 
 pub async fn admin_dashboard(
     session: TypedSession,
@@ -35,13 +28,22 @@ pub async fn admin_dashboard(
     </head>
     <body>
         <p>Welcome {username}!</p>
+        <p>Available actions:</p>
+        <ol>
+            <li><a href="/admin/password">Change password</a></li>
+            <li>
+                <form name="logoutForm" action="/admin/logout" method="post">
+                    <input type="submit" value="Logout">
+                </form>
+            </li>
+        </ol>
     </body>
 </html>"#
         )))
 }
 
 #[tracing::instrument(name = "Get username", skip(pool))]
-async fn get_username(user_id: Uuid, pool: &PgPool) -> Result<String, anyhow::Error> {
+pub async fn get_username(user_id: Uuid, pool: &PgPool) -> Result<String, anyhow::Error> {
     let row = sqlx::query!(
         r#"
         SELECT username
